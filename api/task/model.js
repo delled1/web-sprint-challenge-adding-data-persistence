81a7bb75f26e9642a62aa9481b2736fb89cf7981
Eric Della <ericdella13@gmail.com>
2021-03-07 20:18:30 -0800

    tasks endpoints work

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,38 @@
 // build your `Task` model here
+const db = require("../../data/dbConfig")
+
+async function findTask(id) {
+    return await db("tasks")
+        .where("task_id", id)
+}
+
+//getTasks
+
+async function getTasks() {
+    
+    return await db("Tasks")
+        .select("*")
+        
+}
+
+//addTask
+
+async function addTask(task) {
+
+    const [taskId] = await db("Tasks")
+        .insert(task)
+
+    if (!taskId) {
+        return Promise.resolve(null)
+    }
+
+    const [newProj] = await findTask(taskId)
+
+    return Promise.resolve(newProj)
+}
+
+module.exports = {
+    findTask,
+    getTasks,
+    addTask
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,29 @@
 // build your `/api/tasks` router here
+const express = require("express")
+const tasks= require("./model")
+const router = express.Router()
+
+router.get("/", async (req, res, next) => {
+    try{
+
+        const task = await tasks.getTasks()
+        res.status(200).json(task)
+
+    } catch(err) {
+        next(err)
+    }
+})
+
+
+router.post("/", async (req, res, next) => {
+    try{
+
+        const task = await tasks.addTask(req.body)
+        res.status(201).json(task)
+
+    } catch(err) {
+        next(err)
+    }
+})
+
+module.exports = router
